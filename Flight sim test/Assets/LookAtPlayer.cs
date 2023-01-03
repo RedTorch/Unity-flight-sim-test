b0c67dab80e0829e902aa050449f055f464f0a50
kMed <kenta.medina@protonmail.com>
2023-01-04 00:47:29 +0900

    Initial project

--- Flight sim test/Assets/LookAtPlayer.cs
+++ Flight sim test/Assets/LookAtPlayer.cs
@@ -7,6 +7,7 @@ using TMPro;
 public class LookAtPlayer : MonoBehaviour
 {
     private Transform lookCam;
+    [SerializeField] private Image img;
     [SerializeField] private TMP_Text dist;
     // Start is called before the first frame update
     void Start()
@@ -18,6 +19,15 @@ public class LookAtPlayer : MonoBehaviour
     void Update()
     {
         transform.LookAt(lookCam.position);
-        dist.text = Mathf.Floor(Vector3.Distance(transform.position,lookCam.position)) + "m";
+        float distance = Vector3.Distance(transform.position,lookCam.position);
+        if(distance <= 800f) {
+            dist.text = Mathf.Floor(distance) + "m";
+            img.enabled = true;
+            dist.enabled = true;
+        }
+        else {
+            img.enabled = false;
+            dist.enabled = false;
+        }
     }
 }
